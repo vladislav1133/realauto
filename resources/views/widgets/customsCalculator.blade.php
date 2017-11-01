08b0a9363d1b186a766b2d502e81d8d888930bc7
Mantikor13 <romakrut333bk@gmail.com>
2017-11-01 17:59:45 +0200

    fixed slider

--- resources/views/widgets/customsCalculator.blade.php
+++ resources/views/widgets/customsCalculator.blade.php
@@ -1,5 +1,7 @@
 <div class="calculator">
 
+
+        <div class="calcWrap">
             <!-- Nav tabs -->
             <ul class="nav-tabs" role="tablist">
                 <li role="presentation" class="active"><a href="#light-car" aria-controls="light-car" role="tab" data-toggle="tab"><img src="{{asset('public\carhouse\img\calc-lightCar.png')}}" alt=""></a></li>
@@ -107,4 +109,10 @@
                     </form>
                 </div>
             </div>
+        </div>
+        <div class="calc-img">
+            <img src="carhouse/img/calcMap.png" alt="">
+        </div>
+    </div>
+
 </div>
